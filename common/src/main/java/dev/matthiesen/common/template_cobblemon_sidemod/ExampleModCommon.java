8b15d87150047306afde7bfb89adc9663dfc482e
Adam Matthiesen <amatthiesen@outlook.com>
2026-05-27 17:58:49 -0700

    Add check for Cobblemon mod loading in initialization

--- common/src/main/java/dev/matthiesen/common/template_cobblemon_sidemod/ExampleModCommon.java
+++ common/src/main/java/dev/matthiesen/common/template_cobblemon_sidemod/ExampleModCommon.java
@@ -1,9 +1,15 @@
 package dev.matthiesen.common.template_cobblemon_sidemod;
 
+import dev.matthiesen.common.matthiesen_lib_api.MatthiesenLibApi;
+
 public class ExampleModCommon {
 
     public static void initialize() {
         Constants.createInfoLog("Initialized");
+
+       if (MatthiesenLibApi.isModLoaded("cobblemon")) {
+            Constants.createInfoLog("Cobblemon is loaded, Hello there Cobblemon!");
+       }
     }
 
     public static void onStartup() {
