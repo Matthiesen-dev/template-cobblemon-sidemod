1379b67c5b67b4d5ccfe8d7152fa43308e90fbb8
Adam Matthiesen <amatthiesen@outlook.com>
2026-06-07 14:05:29 -0700

    Update JAR file inclusion/exclusion rules in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,8 @@ tasks.register<Copy>("copyJars") {
     description = "Copies JAR files from fabric and neoforge to output directory"
 
     from("./common/build/libs/") {
-        include("*-sources.jar")
+        include("*.jar")
+        exclude("*-dev-shadow.jar")
     }
     from("./fabric/build/libs/") {
         include("*.jar")
